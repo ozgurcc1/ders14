845a8635d0b32d6d85004afbe6fe16f5bcefa952
Ozgur Celik <ozgurce869@gmail.com>
2022-08-14 17:45:35 +0300

    todos üzerinde usecallback örneği

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-
+import React from 'react';
+import Todos from './Todos';
 
 function App() {
+  const [sayac, sayacGuncelle] = React.useState(0);
+  const [todos, setTodos] = React.useState([]);
+
+  const increment = () => {
+    sayacGuncelle( (oncekiSayac) => oncekiSayac + 1 ); //!!!!!!
+  };
+
+
+  const addTodo = React.useCallback(() => {
+    let saniye = new Date().getSeconds();
+    setTodos( (oncekiDizi) => [...oncekiDizi, "Yapılacak iş "+saniye] ); //!!!!
+  }, []);
+
   return (
-    <p>Merhaba Dünya!</p>
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Sayaç: {sayac}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
   );
 }
 
---
+++ src/Todos.jsx
@@ -0,0 +1,16 @@
+import { memo } from "react";
+
+const Todos = ({ todos }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+      <button>Add Todo</button>
+    </>
+  );
+};
+
+export default memo(Todos);
